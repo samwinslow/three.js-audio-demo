1f119ab9c17548f24caf3b45c13eb0bb11699274
Sam Winslow <sammywinslow@gmail.com>
2020-12-21 17:40:55 -0500

    init

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'class-methods-use-this': 1,
+    'no-empty': 1,
+    'no-fallthrough': 1,
+    'no-nested-ternary': 1,
+    'no-param-reassign': 1,
+    'no-restricted-globals': 1,
+    'no-restricted-properties': 1,
+    'no-shadow': 1,
+    'no-underscore-dangle': 1,
+    'no-unsafe-finally': 1,
+    'prefer-const': 1,
+    'prefer-promise-reject-errors': 1,
+  },
+}

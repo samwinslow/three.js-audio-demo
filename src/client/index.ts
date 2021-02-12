479081261149c3d53d106f7646b80eba5d6f7eb8
Sam Winslow <sammywinslow@gmail.com>
2021-02-12 09:32:20 -0500

    default is plain polysynth

--- src/client/index.ts
+++ src/client/index.ts
@@ -6,7 +6,7 @@ import { majorScales } from './noteDefs'
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-const synth = new Tone.PolySynth(Tone.FMSynth).toDestination()
+const synth = new Tone.PolySynth().toDestination() // Optionally pass synths to PolySynth()
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
